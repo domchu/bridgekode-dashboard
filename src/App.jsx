458297ceca54ce0d533d979a16d62d917657e4eb
Mr.Dominic <ochu.dominic.onah@gmail.com>
2023-07-08 18:31:56 +0100

    chore:installed all the crucial dependebcies

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import {Box} from "@mui/material"
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <Box className="app">
+<h2>Testing the dashboard</h2>
+      </Box>
+    </>
+  )
+}
+
+export default App
