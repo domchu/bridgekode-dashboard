3a94ee4be46115fd1eb16a78a97209ebf598f4c4
Mr.Dominic <ochu.dominic.onah@gmail.com>
2023-07-08 20:29:09 +0100

    chore:added items in json format & geofeatures

---
+++ src/data/mockdata.js
@@ -0,0 +1,34 @@
+export const mockDataTeam = [
+  {
+    id: "1",
+    name: "Ochu Dominic",
+    email: "ochudominic@gmail.com",
+    age: "30",
+    phone: "07034278995",
+    access: "admin",
+  },
+  {
+    id: "2",
+    name: "Ochu Dominic",
+    email: "ochudominic@gmail.com",
+    age: "30",
+    phone: "07034278995",
+    access: "admin",
+  },
+  {
+    id: "3",
+    name: "Ochu Dominic",
+    email: "ochudominic@gmail.com",
+    age: "30",
+    phone: "07034278995",
+    access: "admin",
+  },
+  {
+    id: "4",
+    name: "Ochu Dominic",
+    email: "ochudominic@gmail.com",
+    age: "30",
+    phone: "07034278995",
+    access: "admin",
+  },
+];
