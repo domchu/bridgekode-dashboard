06230d376d0761f44eb79a6ce516ea0a7a7bc97a
Mr.Dominic <ochu.dominic.onah@gmail.com>
2023-07-10 02:06:58 +0100

    chore:done with the topbar structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { colorModeContext, useMode } from "./theme";
 import { Box, CssBaseline, ThemeProvider } from "@mui/material";
+// import Topbar from "./scenes/Global/Topbar";
+import Topbar from "./scenes/Global/Topbar";
 
 function App() {
   const [theme, colorMode] = useMode();
@@ -13,6 +15,7 @@ function App() {
           <Box className="app">
             <main className="content">
               <h1>Testing the dashboard</h1>
+              <Topbar />
             </main>
           </Box>
         </ThemeProvider>
