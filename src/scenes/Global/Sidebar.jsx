b3ed9245de2025c0b802443fff4342734b401b91
Mr.Dominic <ochu.dominic.onah@gmail.com>
2023-07-11 23:48:20 +0100

    chore:imported all the sidebar icons

--- src/scenes/Global/Sidebar.jsx
+++ src/scenes/Global/Sidebar.jsx
@@ -1,9 +1,24 @@
 import React, { useStae } from "react";
 import { ProSidebar, Menu, MenuItem } from "react-pro-sidebar";
-import "react-pro-sidebar/dist/css/styles.css";
+// import "react-pro-sidebar/dist/css/styles.css";
 import { Box, IconButton, useTheme, typography } from "@mui/material";
 import { tokens } from "../../theme";
 import { Link } from "react-router-dom";
+import HomeIcon from "@mui/icons-material/Home";
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
+import PeopleIcon from "@mui/icons-material/People";
+import HelpIcon from "@mui/icons-material/Help";
+import AddIcCallIcon from "@mui/icons-material/AddIcCall";
+import ReceiptLongIcon from "@mui/icons-material/ReceiptLong";
+import EscalatorWarningIcon from "@mui/icons-material/EscalatorWarning";
+import BarChartIcon from "@mui/icons-material/BarChart";
+import PieChartOutlineIcon from "@mui/icons-material/PieChartOutline";
+import DonutLargeIcon from "@mui/icons-material/DonutLarge";
+import LegendToggleIcon from "@mui/icons-material/LegendToggle";
+import MenuIcon from "@mui/icons-material/Menu";
+import TimelineIcon from "@mui/icons-material/Timeline";
+import LocationOnIcon from "@mui/icons-material/LocationOn";
 
 const Sidebar = () => {
   return (
