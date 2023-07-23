32d0352d0c1a6f6a008139a7976a8a1b21680a3d
Mr.Dominic <ochu.dominic.onah@gmail.com>
2023-07-23 13:04:36 +0100

    created the team & imoirted all crucial

--- src/scenes/Team/index.jsx
+++ src/scenes/Team/index.jsx
@@ -1,4 +1,12 @@
 import React from "react";
+import { Typography, IconButton, Box, useTheme } from "@mui/material";
+import { tokens } from "../../theme";
+import { DataGrid } from "@mui/x-data-grid";
+import { mockDataTeam } from "./../../data/mockdata";
+import Header from "../../Components/Header";
+import AdminIcon from "@mui/icons-material/Admin";
+import SecurityIcon from "@mui/icons-material/Security";
+import LockIcon from "@mui/icons-material/Lock";
 
 const Team = () => {
   return <div>Team</div>;
