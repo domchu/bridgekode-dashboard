2817291bcfbb346e177d0b4ab7b380992f1f76df
Mr.Dominic <ochu.dominic.onah@gmail.com>
2023-07-24 01:59:22 +0100

    chore:imported line chart to its folder

--- src/scenes/Line/index.jsx
+++ src/scenes/Line/index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Header from "../../Components/Header";
+import LineChart from "../../Components/LineChart";
 import { Box } from "@mui/material";
 
 const Line = () => {
@@ -7,6 +8,9 @@ const Line = () => {
     <>
       <Box m="20px">
         <Header title="LINE CHART" subtitle="Simple Line Chart " />
+        <Box height="75vh">
+          <LineChart />
+        </Box>
       </Box>
     </>
   );
