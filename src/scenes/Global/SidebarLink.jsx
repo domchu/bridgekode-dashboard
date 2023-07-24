0ff7a0ad188ba3d815613ea1c7cecc78cbec4304
Mr.Dominic <ochu.dominic.onah@gmail.com>
2023-07-24 02:31:56 +0100

    chore:minor changes at sidebar

--- src/scenes/Global/SidebarLink.jsx
+++ src/scenes/Global/SidebarLink.jsx
@@ -32,7 +32,7 @@ const Item = ({ title, to, icon, selected, setSelected }) => {
         onClick={() => setSelected(title)}
         icon={icon}
       >
-        <Typography>{title} </Typography>
+        <Typography>{title}</Typography>
         <Link to={to} />
       </MenuItem>
     </>
